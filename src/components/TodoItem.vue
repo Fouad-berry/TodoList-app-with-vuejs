cfeaf80830e566d636b45011beae69a0aa89dfe7
Fouad-berry <fidriss96@gmail.com>
2025-04-29 23:14:33 +0200

    [ADD] add TodoItem cotent

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <li class="todo-item">
+      <input type="checkbox" :checked="todo.done" @change="$emit('toggle', todo.id)" />
+      <span :class="{ done: todo.done }">{{ todo.title }}</span>
+      <button @click="$emit('remove', todo.id)">🗑️</button>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      todo: Object
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .done {
+    text-decoration: line-through;
+  }
+  .todo-item {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin-bottom: 5px;
+  }
+  </style>
+  
